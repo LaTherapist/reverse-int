7651786794aee48b81ea8fb4bebe7e00cd0da60e
LaTherapist <arztpsychiater@gmail.com>
2021-02-03 18:41:41 +0200

    feat: add changeNumber function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+let changeNumber = (n) => Number(String(n).split('').reverse().join(''));
+
 module.exports = function reverse (n) {
-  
+   return n < 0 ? changeNumber(n * -1) : changeNumber(n);
 }
